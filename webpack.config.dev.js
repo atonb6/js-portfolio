6c0a6fefd6ef6a4ea891f993b747da7847227a64
Philippe Paris <contacto@philippeparis.net>
2022-08-29 17:50:32 -0400

    update

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,26 +1,28 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const CopyPlugin = require('copy-webpack-plugin');
 const Dotenv = require('dotenv-webpack');
+const {BundleAnalyzerPlugin} = require('webpack-bundle-analyzer');
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: './src/index.js',
   output: {
-    path: path.resolve(__dirname, "dist"),
-    filename: "[name][contenthash].js",
-    assetModuleFilename: 'assets/images/[hash][ext][query]'
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name][contenthash].js',
+    assetModuleFilename: 'assets/images/[hash][ext][query]',
   },
   mode: 'development',
-  watch: true,
+  devtool: 'source-map',
+/*   watch: true, */
   resolve: {
-    extensions: [".js"],
+    extensions: ['.js'],
     alias: {
       '@utils': path.resolve(__dirname, 'src/utils/'),
       '@templates': path.resolve(__dirname, 'src/templates/'),
       '@styles': path.resolve(__dirname, 'src/styles/'),
-      '@images': path.resolve(__dirname, 'src/assets/images/')
-    }
+      '@images': path.resolve(__dirname, 'src/assets/images/'),
+    },
   },
   module: {
     rules: [
@@ -28,19 +30,16 @@ module.exports = {
         test: /\.m?js$/,
         exclude: /node_modules/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
         },
       },
       {
         test: /\.css|.styl$/,
-        use: [MiniCssExtractPlugin.loader,
-          'css-loader',
-          'stylus-loader'
-        ],
+        use: [MiniCssExtractPlugin.loader, 'css-loader', 'stylus-loader'],
       },
       {
         test: /\.png/,
-        type: 'asset/resource'
+        type: 'asset/resource',
       },
       {
         test: /\.(woff|woff2)$/,
@@ -49,42 +48,50 @@ module.exports = {
           options: {
             limit: 10000, // O LE PASAMOS UN BOOLEANOS TRUE O FALSE
             // Habilita o deshabilita la transformación de archivos en base64.
-           mimetype: 'aplication/font-woff',
-           // Especifica el tipo MIME con el que se alineará el archivo. 
-           // Los MIME Types (Multipurpose Internet Mail Extensions)
-           // son la manera standard de mandar contenido a través de la red.
-           name: "[name].[contenthash].[ext]",
-           // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
-           // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria 
-           // ubuntu-regularhola.woff
-           outputPath: './assets/fonts/', 
-           // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
-           publicPath: '../assets/fonts/',
-           // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
-          esModule: false 
-         // AVISAR EXPLICITAMENTE SI ES UN MODULO
-    }
-        }
-      }
-    ]
+            mimetype: 'aplication/font-woff',
+            // Especifica el tipo MIME con el que se alineará el archivo.
+            // Los MIME Types (Multipurpose Internet Mail Extensions)
+            // son la manera standard de mandar contenido a través de la red.
+            name: '[name].[contenthash].[ext]',
+            // EL NOMBRE INICIAL DEL ARCHIVO + SU EXTENSIÓN
+            // PUEDES AGREGARLE [name]hola.[ext] y el output del archivo seria
+            // ubuntu-regularhola.woff
+            outputPath: './assets/fonts/',
+            // EL DIRECTORIO DE SALIDA (SIN COMPLICACIONES)
+            publicPath: '../assets/fonts/',
+            // EL DIRECTORIO PUBLICO (SIN COMPLICACIONES)
+            esModule: false,
+            // AVISAR EXPLICITAMENTE SI ES UN MODULO
+          },
+        },
+      },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      inject: true,
+      inject: 'body',
       template: './public/index.html',
-      filename: './index.html'
+      filename: './index.html',
     }),
     new MiniCssExtractPlugin({
-      filename: 'assets/[name].[contenthash].css'
+      filename: 'assets/[name].[contenthash].css',
     }),
     new CopyPlugin({
       patterns: [
         {
-          from: path.resolve(__dirname, "src", "assets/images"),
-          to: "assets/images"
-        }
-      ]
+          from: path.resolve(__dirname, 'src', 'assets/images'),
+          to: 'assets/images',
+        },
+      ],
     }),
     new Dotenv(),
+    new BundleAnalyzerPlugin(),
   ],
+  devServer: {
+		static: path.join(__dirname, 'dist'),
+		compress: true,
+		historyApiFallback: true,
+		port: 8080,
+		open: true, 
+  },
 };
